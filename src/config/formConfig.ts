a70bfc24c52faeee25ff1663909bf246a4d4160b
marned91 <152280051+marned91@users.noreply.github.com>
2025-06-11 16:22:09 +0200

    chore: setup Vite + React + TS + Tailwind + config

---
+++ src/config/formConfig.ts
@@ -0,0 +1,56 @@
+export const formConfig = {
+  title: 'Job Application Form',
+  fields: [
+    {
+      label: 'Full Name',
+      name: 'fullName',
+      type: 'text',
+      required: true,
+      minLength: 3,
+      errorMessage: 'Full name must be at least 3 characters.',
+    },
+    {
+      label: 'Email Address',
+      name: 'email',
+      type: 'email',
+      required: true,
+      pattern: '^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$',
+      errorMessage: 'Please enter a valid email',
+    },
+    {
+      label: 'Desired position',
+      name: 'position',
+      type: 'select',
+      required: true,
+      options: [
+        'Frontend developer',
+        'Backend developer',
+        'UX Designer',
+        'Marketing',
+        'Operations',
+        'Quality Assurance',
+      ],
+    },
+    {
+      label: 'Portfolio URL',
+      name: 'portfolio',
+      type: 'text',
+      requiredIf: {
+        position: ['Frontend developer', 'UX Designer'],
+      },
+    },
+    {
+      label: 'Motivational Letter',
+      name: 'motivation',
+      type: 'textarea',
+      required: true,
+      minLength: 20,
+      maxLength: 120,
+    },
+    {
+      label: 'Prefer to work from home',
+      name: 'homeOffice',
+      type: 'checkbox',
+    },
+  ],
+};
---
+++ src/types/formTypes.ts
@@ -0,0 +1,23 @@
+export type FieldType = 'text' | 'email' | 'checkbox' | 'textarea' | 'select';
+
+export type RequiredIf = {
+  [fieldName: string]: string[];
+};
+
+export type FormField = {
+  label: string;
+  name: string;
+  type: FieldType;
+  required?: boolean;
+  options?: string[];
+  pattern?: string;
+  minLength?: number;
+  maxLength?: number;
+  errorMessage?: string;
+  requiredIf?: RequiredIf;
+};
+
+export type FormConfig = {
+  title: string;
+  fields: FormField[];
+};
